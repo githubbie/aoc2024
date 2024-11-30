ef054b71408edd6b7f0c9af302991514624ee9ca
Remco van Engelen <remco.van.engelen@gmail.com>
2024-11-30 09:17:56 +0100

    setup

---
+++ day-0.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+try:
+  DAY=int(sys.argv[0].split('-')[-1].replace('.py',''))
+except:
+  DAY=0
+
+def process(filename):
+    for line in open(filename):
+        line = line.strip()
+        print(line)
+
+test = f'input/test-{DAY}.txt'
+real = f'input/day-{DAY}.txt'
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == '-t':
+        process(test)
+    elif sys.argv[1] == '-r':
+        process(real)
+    else:
+        process(sys.argv[1])
+else:
+    if os.path.exists(test):
+        process(test)
+    if os.path.exists(real):
+        process(real)
